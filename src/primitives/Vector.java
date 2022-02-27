6bb75d35dfa9fa93ef89b343d8041defaddfe84e
Aviad Shlosberg <ashlosbe@g.jct.ac.il>
2022-02-27 15:16:00 +0200

    Technical changes

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -12,12 +12,11 @@ public class Vector extends Point
         return xyz.d1 * xyz.d1 + xyz.d2* xyz.d2 + xyz.d3* xyz.d3;
     }
 
-    public double length()
-    {
+    public double length() {
         return Math.sqrt(lengthSquared());
     }
-    public Vector add(Vector myVec)
-    {
+
+    public Vector add(Vector myVec) {
         return new Vector(this.xyz.add(myVec.xyz));
     }
 
@@ -26,30 +25,24 @@ public class Vector extends Point
         return new Vector(this.xyz.scale(scalar));
     }
 
-    public double dotProduct(Vector myVec)
-    {
+    public double dotProduct(Vector myVec) {
         return this.xyz.d1*myVec.xyz.d1 +this.xyz.d2*myVec.xyz.d2 +this.xyz.d3*myVec.xyz.d3;
     }
 
-    public Vector crossProduct(Vector myVec)
-    {
+    public Vector crossProduct(Vector myVec) {
        return new Vector(this.xyz.d2*myVec.xyz.d3-this.xyz.d3*myVec.xyz.d2,
                         this.xyz.d3*myVec.xyz.d1-this.xyz.d1*myVec.xyz.d3,
                         this.xyz.d1*myVec.xyz.d2-this.xyz.d2*myVec.xyz.d1);
-
     }
 
-    public Vector normalize()
-    {
+    public Vector normalize() {
         Double3 p = xyz.scale(length());
         return new Vector(p.d1,p.d2,p.d3);
     }
-
 
     @Override
     public String toString() {
         return "Vector{}";
     }
-
 
 }
