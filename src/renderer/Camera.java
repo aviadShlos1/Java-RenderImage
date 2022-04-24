3bb04d94524d17a000757e66832a26f442e710fb
Aviad Shlosberg <ashlosbe@g.jct.ac.il>
2022-04-24 13:50:03 +0300

    Fix image tar 5

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -103,6 +103,46 @@ public class Camera {
         return new Ray(P0, Vij);
     }
 
+    public void renderImage() {
+        if (imageWriter == null)
+            throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
+        if (rayTracerBasic == null)
+            throw new MissingResourceException("Error: you missed", "Camera", "rayTracerBasic");
+        for (int i=0; i< imageWriter.getNx();i++){
+            for (int j=0; j< imageWriter.getNy();j++){
+                imageWriter.writePixel(j,i,castRay(j,i));
+            }
+        }
+    }
+
+    public void printGrid(int interval, Color color) {
+        if (imageWriter == null)
+            throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
+
+        for (int i=0; i< imageWriter.getNx();i++){
+            for (int j=0; j< imageWriter.getNy();j++){
+                if(i%interval==0 && i!=0 || j%interval==0 && j!=0)
+                    imageWriter.writePixel(i,j,color);
+            }
+        }
+    }
+
+    public void writeToImage() {
+        if (imageWriter == null)
+            throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
+        imageWriter.writeToImage();
+    }
+
+    private Color castRay(int j,int i)
+    {
+        Ray rayForCast=constructRayThroughPixel(imageWriter.getNx(), imageWriter.getNy(), j,i);
+        return rayTracerBasic.traceRay(rayForCast);
+    }
+
+
+
+
+
 ////////////// getters & setters
 
     /**
@@ -199,49 +239,5 @@ public class Camera {
     public Camera setRayTracer(RayTracerBasic rayTracerBasic) {
         this.rayTracerBasic=rayTracerBasic;
         return this;
-    }
-
-    public void renderImage() {
-            if (imageWriter == null)
-                throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
-            if (rayTracerBasic == null)
-                throw new MissingResourceException("Error: you missed", "Camera", "rayTracerBasic");
-            throw new UnsupportedOperationException();
-
-        for (int i=0; i< imageWriter.getNx();i++){
-            for (int j=0; j< imageWriter.getNy();j++){
-                imageWriter.writePixel(i,j,castRay(j,i));
-            }
-        }
-
-    }
-
-    public void printGrid(int interval, Color color) {
-        if (imageWriter == null)
-            throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
-
-        for (int i=0; i< imageWriter.getNx();i++){
-            for (int j=0; j< imageWriter.getNy();j++){
-                if(i%interval==0||j%interval==0)
-                    imageWriter.writePixel(i,j,color);
-            }
-        }
-//        for (int i=0; i< imageWriter.getNx();i++){
-//            for (int j=0; j< imageWriter.getNy();j+=interval){
-//                imageWriter.writePixel(i,j,color);
-//            }
-//        }
-    }
-
-    public void writeToImage() {
-        if (imageWriter == null)
-            throw new MissingResourceException("Error: you missed", "Camera", "imageWriter");
-        imageWriter.writeToImage();
-    }
-
-    private Color castRay(int j,int i)
-    {
-        Ray rayForCast=constructRayThroughPixel(imageWriter.getNx(), imageWriter.getNy(), j,i);
-        return rayTracerBasic.traceRay(rayForCast);
     }
 }
--- src/renderer/RayTracerBase.java
+++ src/renderer/RayTracerBase.java
@@ -3,13 +3,27 @@ package renderer;
 import primitives.Color;
 import primitives.Ray;
 import scene.Scene;
-
+/**
+ * An abstract class used to trace rays for the rendering engine
+ */
 public abstract class RayTracerBase {
+    /**
+     * scene to be rendered
+     */
     protected Scene scene;
 
+    /**
+     * constructor for the ray tracer
+     * @param scene to be intersected
+     */
     public RayTracerBase(Scene scene) {
         this.scene = scene;
     }
+    /**
+     * abstract function to determine the color of a pixel
+     * @param ray - ray to intersect
+     * @return the color of the pixel intersects the given ray
+     */
     public Color traceRay(Ray ray){
         return null;
     }
