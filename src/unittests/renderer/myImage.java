f7551d6398982aa7d4d07abdda6c01a3d5686540
Aviad Shlosberg <ashlosbe@g.jct.ac.il>
2022-06-06 18:43:59 +0300

    RoomWith4Balls

--- src/unittests/renderer/myImage.java
+++ src/unittests/renderer/myImage.java
@@ -14,14 +14,16 @@ import scene.Scene;
 import java.util.Random;
 
 
-public class myImage {
+public class myImage
+{
     Scene myScene = new Scene("firstImage");
 
     /**
      * Creates a room image with a couple of elements using lights, shadows and refraction techniques.
      */
     @Test
-    public void createFirstImage() {
+    public void createFirstImage()
+    {
         Camera camera3 = new Camera(new Point(0, 50, 160),
                 new Vector(0, 0, -1), new Vector(0, 1, 0)) //
                 .setViewPlaneSize(200, 200) //
@@ -354,177 +356,52 @@ public class myImage {
                 setShininess(100));
         //endregion plate
 //endregion table
+// region table decorations
 
-        //region lights
+        //decor outer
+        Geometry sphere1 = new Sphere(new Point(-52,24,76), 2d)
+                .setEmission(new Color(BLUE)) //
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKs(0.8)
+                        .setShininess(100)
+                );
+        Geometry sphere2 = new Sphere(new Point(-57,24,74), 2d)
+                .setEmission(new Color(RED)) //
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKs(0.8)
+                        .setShininess(80));
+        Geometry sphere3 = new Sphere(new Point(-53.5,24,80), 2d)
+                .setEmission(new Color(YELLOW)) //
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKs(0.8)
+                        .setShininess(10));
+        Geometry sphere4 = new Sphere(new Point(-55,24,71), 2d)
+                .setEmission(new Color(GREEN)) //
+                .setMaterial(new Material()
+                        .setKd(0.2)
+                        .setKs(0.8)
+                        .setShininess(10));
+        //endregion table decorations
+//region lights
         myScene.lights.add(new PointLight(new Color(java.awt.Color.YELLOW)
                 .add(new Color(java.awt.Color.YELLOW)).scale(0.2), new Point(0, 50, 40),3));
         myScene.lights.add(new SpotLight(new Color(java.awt.Color.orange), new Point(-56, 50, 71),new Vector(0,-1,0),3));
         myScene.lights.add(new SpotLight(new Color(java.awt.Color.CYAN).scale(0.8), new Point(10, 55, 75),new Vector(1.5,-1,0),3));
 //endregion lights
 
-            myScene.geometries.add(door1,middle1,middle2,door2,door3,door4,door5,middle3,floor,wallBehind,handle1,handle2,handle3,handle4,roof
-                    ,wallRight,wallLeft,foot11,foot12,foot13,foot14,foot21,foot22,foot23,foot24,foot31,foot32,foot33,foot34,foot41,foot42,foot43,foot44,
-                    plateUp,plateDown,plateSide1,plateSide2,plateSide3,plateSide4,middle4,door6,wallFront);
+        myScene.geometries.add(floor,wallBehind,roof,wallFront
+                ,wallRight,wallLeft,foot11,foot12,foot13,foot14,foot21,foot22,foot23,foot24,foot31,foot32,foot33,foot34,foot41,foot42,foot43,foot44,
+                plateUp,plateDown,plateSide1,plateSide2,plateSide3,plateSide4,sphere1,sphere2,sphere3,sphere4);
 
         camera3.setImageWriter(new ImageWriter("Room", 1000, 1000))
                     .setRayTracer(new RayTracerBasic(myScene)
                             .setMIN_SHADOW_POINTS(10));
             camera3.renderImageWithTreads();
             camera3.writeToImage();
-        }
     }
-//        myScene.geometries.add(//
-//                // The flat table
-//                new Polygon(
-//                        new Point(100, 0, -100),
-//                        new Point(0, 100, -100),
-//                        new Point(-100, 0, -100),
-//                        new Point(0, -100, -100))
-//                        .setEmission(new Color(0, 128, 0))//
-//                        .setMaterial(new Material()
-//                                .setKd(0.5)
-//                                .setKs(0.5)
-//                                .setShininess(60)), ///
-//
-//                new Sphere(new Point(0, 56, -50), 7d)
-//                        .setEmission(new Color(BLACK)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//
-//                new Sphere(new Point(7, 44, -50), 7d)
-//                        .setEmission(new Color(BLUE)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))
-//                        ),
-//
-//                new Sphere(new Point(-7, 44, -50), 7d)
-//                        .setEmission(new Color(RED)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//
-//                new Sphere(new Point(-14, 31, -50), 7d)
-//                        .setEmission(new Color(YELLOW)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//
-//                new Sphere(new Point(-0.5, 31, -50), 7d)
-//                        .setEmission(new Color(0, 153, 230)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//
-//                new Sphere(new Point(14, 31, -50), 7d)
-//                        .setEmission(new Color(102, 0, 102)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//
-//                new Sphere(new Point(-0.5, -10, -25), 7d)
-//                        .setEmission(new Color(WHITE)) //
-//                        .setMaterial(new Material()
-//                                .setKd(0.2)
-//                                .setKs(0.8)
-//                                .setShininess(10)
-//                                .setKt(new Double3(0.6))),
-//                //The starter
-//                new Cylinder(3, new Ray(new Point(-0.5, -100, -25), new Vector(0, 4, 1)), 60)
-//                        .setMaterial(new Material()
-//                                .setShininess(100)
-//                                .setKd(0.7)
-//                                .setKs(0.5)
-//                                .setKt(0.4))
-//                        .setEmission(new Color(77, 40, 0)),
-//                //edge right
-//                new Cylinder(1.5, new Ray(new Point(-0.5, 69, 0), new Vector(0.55, -0.88, 0)), 56)
-//                        .setMaterial(new Material()
-//                                .setShininess(100)
-//                                .setKd(0.7)
-//                                .setKs(0.5)
-//                                .setKt(0.4))
-//                        .setEmission(new Color(51, 26, 0).reduce(2)),
-//                //edge left
-//                new Cylinder(1.5, new Ray(new Point(-27, 23, 2), new Vector(3, 5, 2)), 55)
-//                        .setMaterial(new Material()
-//                                .setShininess(100)
-//                                .setKd(0.7)
-//                                .setKs(0.5)
-//                                .setKt(0.4))
-//                        .setEmission(new Color(51, 26, 0).reduce(2)),
-//                //edge bottom
-//                new Cylinder(1.5, new Ray(new Point(-29, 22, -25), new Vector(10, 0, 1)), 60)
-//                        .setMaterial(new Material()
-//                                .setShininess(100)
-//                                .setKd(0.7)
-//                                .setKs(0.5)
-//                                .setKt(0.4))
-//                        .setEmission(new Color(51, 26, 0).reduce(2)),
-//
-//
-//                //region moon
-//
-//                new Sphere(new Point(-75, 80, 80), 10)
-//                        .setMaterial(new Material()
-//                                .setKd(0.005)
-//                                .setKs(0.00005)
-//                                .setShininess(50)
-//                                .setKt(0.9))
-//                        .setEmission(Color.YELLOW.add(Color.WHITE.reduce(6))),
-//
-//
-//                //endregion
-//
-//                //region stars
-//                new Sphere(new Point(-75, 60, 80), 2)
-//                        .setMaterial(new Material()
-//                                .setKd(0.005)
-//                                .setKs(0.00005)
-//                                .setShininess(100)
-//                                .setKt(0.9))
-//                        .setEmission(Color.YELLOW.reduce(4)));
-//
-//
-//        //moon
-//        myScene.lights.add(
-//                new PointLight(
-//                        Color.YELLOW.reduce(6),
-//                        new Point(-75, 80, 80)));
-//
-//        myScene.lights.add( ///
-//                new SpotLight(new Color(YELLOW), new Point(10, 20, -10), new Vector(1, 1, 0), 5) //
-//                        .setkL(0.0004)
-//                        .setkQ(0.000006));
-//
-//        myScene.lights.add(
-//                new PointLight(new Color(WHITE).reduce(3.2), new Point(10, 0, 15), 4)
-//                        .setkL(0.00005)
-//                        .setkQ(0.0000012));
-//
-//        myScene.lights.add(
-//                new DirectionalLight(new Color(GRAY).reduce(10), new Vector(1, 1, -10)));
-//
-//
-//        camera.setImageWriter(new ImageWriter("BilliardWithAA_SH", 500, 500))
-//                .setRayTracer(
-//                        new RayTracerBasic(myScene)
-//                                .setMIN_SHADOW_POINTS(30)); //
-//        camera.renderImageWithTreads(); //
-//        camera.writeToImage();
-//    }
+}
 
 
